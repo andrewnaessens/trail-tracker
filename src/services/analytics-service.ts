c7e817ba1da911b70c1efd3e53abe356c59aeece
Andrew Naessens <195749017+andrewnaessens@users.noreply.github.com>
2026-05-24 16:15:53 +0100

    chore: convert services from JS to TS

--- src/services/analytics-service.ts
+++ src/services/analytics-service.ts
@@ -1,16 +1,16 @@
 import { db } from "../models/db.js";
 
 export const analyticsService = {
-  getUserAnalytics: async function (userId) {
+  getUserAnalytics: async function (userId: string) {
     const user = await db.userStore.getUserById(userId);
-    const categories = await db.categoryStore.getUserCategories(user);
+    const categories = await db.categoryStore.getUserCategories(user) || [];
     let categoryId = null;
     let categoryTrails = null;
     const trails = [];
     for (let i = 0; i < categories.length; i += 1) {
       categoryId = categories[i]._id;
       // eslint-disable-next-line no-await-in-loop
-      categoryTrails = await db.trailStore.getTrailsByCategoryId(categoryId);
+      categoryTrails = await db.trailStore.getTrailsByCategoryId(categoryId) || [];
       trails.push(...categoryTrails);
     };
     const stats = {
@@ -22,12 +22,12 @@ export const analyticsService = {
     return stats;
   },
 
-  getAllUserAnalytics: async function (allUsers) {
+  getAllUserAnalytics: async function (allUsers: any) {
     const stats = [];
     for (let i = 0; i < allUsers.length; i += 1) {
       const user = allUsers[i]
       // eslint-disable-next-line no-await-in-loop
-      const categories = await db.categoryStore.getUserCategories(user._id);
+      const categories = await db.categoryStore.getUserCategories(user._id) || [];
       
       let categoryId = null;
       let categoryTrails = null;
@@ -35,7 +35,7 @@ export const analyticsService = {
       for (let j = 0; j < categories.length; j += 1) {
         categoryId = categories[j]._id;
         // eslint-disable-next-line no-await-in-loop
-        categoryTrails = await db.trailStore.getTrailsByCategoryId(categoryId);
+        categoryTrails = await db.trailStore.getTrailsByCategoryId(categoryId) || [];
         trails.push(...categoryTrails);
       };
       stats.push({
